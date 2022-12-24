1fabe5f8f1ba6629c98b6adfe812a338ff5fa3da
Max Somerville <max.somerville62@gmail.com>
2022-12-25 11:23:41 +1300

    game service implemented

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,21 +3,22 @@ import {useEffect, useState} from "react";
 import {io} from "socket.io-client";
 import axios from "axios";
 
-//For testing purposes
-const userID = 1;
-
 const game_service ="http://localhost:4004/";
 const socket = io(game_service, {
   autoConnect: false, //Will need to open socket connection using socket.connect();
 });
 
 function App() {
+  const [gameState, setGameState] = useState(null);
+  const [userId, setuserId] = useState(0);
+  const [temp, setTemp] = useState(0);
   const [auth, setauth] = useState(false);
 
   useEffect(() => {
 
     socket.on('auth-ack', (data) => {
       if("valid" in data) {
+        console.log("AUTH == " + data.valid);
         setauth(data.valid);
 
       } else {
@@ -25,13 +26,30 @@ function App() {
       }
     });
 
-    socket.on('message', (data) => {
+    socket.on('game-start', (data) => {
+      console.log(data);
+      setGameState(data);
+    });
+
+    socket.on('join-queue', (data) => {
       console.log(data);
     });
+
+    socket.on("game-update", (data) => {
+      console.log(data);
+      setGameState(data); //Overwrites the current game state
+    });
+
+    socket.on("game-over", (data) => {
+      console.log(data);
+      alert("The game is over!");
+    })
   
     return () => {
       socket.off("auth-ack");
-      socket.off("message");
+      socket.off("join-queue");
+      socket.off("game-start");
+      socket.off("game-upate");
     }
   }, [socket]);
   
@@ -43,17 +61,25 @@ function App() {
   }
 
   const authenticateWithServer = () => {
-    console.log("Sending auth");
+    console.log("Sending auth : " + userId);
     //sends the user id to the socket server using the authenticate 
     socket.emit("authenticate", {
-      userId: userID, //<---- for now its just user ID but could send a user JWT token
+      userId: userId, //<---- for now its just user ID but could send a user JWT token
     });
   }
 
   const joinQueue = () => {
-    axios.post('http://localhost:4004/game/queue/1').then(res => {
+    axios.post('http://localhost:4004/game/queue/'+userId).then(res => {
       console.log(res);
     });
+  }
+
+  const makeTurn = () => {
+    if(gameState !== null) {
+      socket.emit("game-turn", gameState);
+    } else {
+      console.log("Not in game!");
+    }
   }
 
   
@@ -64,6 +90,9 @@ function App() {
         <button onClick={authenticateWithServer}>Authenticate</button>
         <h3> we are auth : {auth ? "true" : "false"}</h3>
         <button onClick={joinQueue}>Join queue</button>
+        <input onChange={(e) => {setTemp(e.target.value)}}/>
+        <button onClick={() => {setuserId(temp)}}>Set userId</button>
+        <button onClick={makeTurn}>updateGame</button>
       </header>
     </div>
   );
